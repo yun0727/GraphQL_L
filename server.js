e60462daf4ba48070900485a430f30b21f4cedc1
yun <austru00@gmail.com>
2024-03-19 20:36:24 +0900

    4.2 Scalar and Root Types

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 import { ApolloServer, gql } from "apollo-server"
 
 const typeDefs = gql`
-  type Query {
+  type User {
+    id: ID
+    username: String
+  }
+  type Tweet {
+    id: ID
     text: String
-    hello: String
-  }`
+    author: User
+  }
+  type Query {
+    allTweets: [Tweet]
+    tweet(
+      id:ID
+      ): Tweet
+  }
+  `
 
 const server = new ApolloServer({typeDefs
   });
