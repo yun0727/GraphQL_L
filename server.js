e2d46a8476cf8e14588c6d179cf02868174a8ca8
yun <austru00@gmail.com>
2024-03-20 20:47:42 +0900

    4.4 Non Nullable Fields

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ import { ApolloServer, gql } from "apollo-server"
 
 const typeDefs = gql`
   type User {
-    id: ID
-    username: String
+    id: ID!
+    username: String!
+    firstName: String!  
+    lastName: String!
   }
   type Tweet {
     id: ID!
@@ -14,7 +16,7 @@ const typeDefs = gql`
     allTweets: [Tweet!]!
     tweet(
       id:ID!
-      ): Tweet!
+      ): Tweet
   }
   type Mutation{
     postTweet(text: String!, userId: ID!): Tweet!
