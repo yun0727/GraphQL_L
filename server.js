f9edaef2f05f0cd1340cd605e256881cc47bf761
yun <austru00@gmail.com>
2024-03-22 17:57:40 +0900

    4.7 Mutation Resolvers

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import { ApolloServer, gql } from "apollo-server"
 
-const tweets = [
+let tweets = [
   {
     id: "1",
     text:"first one!",
@@ -42,6 +42,22 @@ const resolvers = {
     },
     tweet(root, {id}){
       return tweets.find((tweet)=>tweet.id === id)
+    }
+  },
+  Mutation:{
+    postTweet(_,{text, userId}){
+      const newTweet = {
+        id: tweets.length+1,
+        text,
+      };
+      tweets.push(newTweet);
+      return newTweet
+    },
+    deleteTweet(_,{id}){
+      const Tweet = tweets.find((tweet)=>tweet.id === id)
+      if (!tweet) return false;
+      tweets = tweets.filter((tweet)=>tweet.id !== id)
+      return true
     }
   }
 }
